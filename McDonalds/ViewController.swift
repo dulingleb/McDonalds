525579d558c1feae37b695950b4e0456b7b9910d
dulingleb <dulingleb@gmail.com>
2023-09-21 19:57:48 +0200

    Initial Commit

---
+++ McDonalds/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  McDonalds
+//
+//  Created by Dulin Gleb on 21.9.23..
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
